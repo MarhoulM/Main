1e11c58f4774d7e7a228cccf2733e88247946f18
Michal Marhoul <marhoul.m@seznam.cz>
2025-07-03 23:22:28 +0200

    Add htaccess, update Form

--- Portfolio_form/Form.php
+++ Portfolio_form/Form.php
@@ -10,6 +10,7 @@ $dotenv->load();
 use PHPMailer\PHPMailer\PHPMailer;
 use App\FormHandler;
 
+/*
 header("Access-Control-Allow-Origin: https://portfolio-g26i.onrender.com");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
@@ -19,6 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+*/
+
+header("Content-Type: application/json; charset=UTF-8");
 
 $response = [];
 
