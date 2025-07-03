706d5195d3ffa65a97200cb099522b2e5f654a0e
Michal Marhoul <marhoul.m@seznam.cz>
2025-07-03 22:04:09 +0200

    Update CORS

--- Portfolio_form/Form.php
+++ Portfolio_form/Form.php
@@ -10,7 +10,7 @@ $dotenv->load();
 use PHPMailer\PHPMailer\PHPMailer;
 use App\FormHandler;
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://portfolio-g26i.onrender.com");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
