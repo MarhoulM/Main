667765efcebb56b48c9e3ad096d98b549a96beb7
Michal Marhoul <marhoul.m@seznam.cz>
2025-08-17 23:04:05 +0200

    Add PatchProductDto and update Dockerfile

---
+++ evidence/DTOs/PatchProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Evidence.DTOs
+{
+    public class PatchProductDto
+    {
+        public int Id { get; set; }
+        public string Borrowed { get; set; }
+        public bool Availability { get; set; }
+    }
+}
---
+++ evidence/Migrations/20250815144859_ProductBorrowed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evidence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductBorrowed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Borrowed",
+                table: "Products",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Borrowed",
+                table: "Products");
+        }
+    }
+}
